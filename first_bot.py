9863a374397b8b7cf5b1e763820a95170514af3b
Ambartsumyan Karen <kar.amb@yandex.ru>
2018-02-18 17:20:13 +0300

    Изменения логирования.

--- first_bot.py
+++ first_bot.py
@@ -1,7 +1,7 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 
-logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
                     filename='bot.log'
                     )
@@ -15,17 +15,18 @@ def main():
     updater.start_polling()
     updater.idle()
 
-
+#функция обработки комманды /start
 def user_welcomming(bot, update):
-    my_text = """Здарова {}!
-Я только начал жить, не суди строго =)""".format(update.message.chat.first_name)
+    my_text = "Здарова {}! Я только начал жить, не суди строго =)".format(update.message.chat.first_name)
     print(my_text)
     update.message.reply_text(my_text)
 
+#Функция общения с пользователем
 def talking_with_user(bot, update):
     user_text = update.message.text
-    print(user_text)
+    logging.info(user_text) #Логируем все что пишет юзер
     update.message.reply_text(user_text+'. Окей, и что?')
 
 if __name__ == "__main__":
+    logging.info('Bot has been started') #Сообщение о старте успешном бота в логе
     main()
